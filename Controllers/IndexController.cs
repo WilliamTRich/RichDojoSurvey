27e715232b6e217817ab582e05d4cf807e3178ef
William Rich <williamrich@homerak.net>
2022-12-08 14:18:32 -0800

    Staging Project

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+// Be sure to use your own project's namespace here! 
+namespace IndexController.Controllers;
+public class IndexController : Controller   // Remember inheritance?    
+{
+    [HttpGet] // We will go over this in more detail on the next page    
+    [Route("")] // We will go over this in more detail on the next page
+    public ViewResult Index()
+    {
+        return View("Index");
+    }
+
+    [HttpGet] // We will go over this in more detail on the next page    
+    [Route("view")] // We will go over this in more detail on the next page
+    public ViewResult ViewRes(string name, string dojo, string flang, string comment)
+    {
+        ViewBag.Name = name;
+        ViewBag.Dojo = dojo;
+        ViewBag.Flang = flang;
+        ViewBag.Comment = comment;
+        return View("View");
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(string name, string dojo, string flang, string comment)
+    {
+        return RedirectToAction("ViewRes", new { name = name, dojo = dojo, flang = flang, comment = comment });
+    }
+}
